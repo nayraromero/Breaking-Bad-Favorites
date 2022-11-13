aecace57e7136b9f0744b467116d83897f208f75
nayraromero <nayraromero@gmail.com>
2022-11-13 15:31:21 +0100

    show favorite cards and keep it in local Storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-console */
 'use strict';
 // //VARIABLES
-// // const btn = document.querySelector('.btn-search');
-// // const input = document.querySelector('.js-input');
+const btn = document.querySelector('.btn-search');
+const input = document.querySelector('.js-input');
 const characterListEl = document.querySelector('.js-list');
 const favoritesListEl = document.querySelector('.js-list-favorites');
+const form = document.querySelector('.form');
 let allCharacters = [];
 let favoriteCharacters = [];
 
@@ -21,10 +22,10 @@ function renderOneCharacter(oneCharacter) {
   return html;
 }
 
-function renderAllCharacters() {
+function renderAllCharacters(character) {
   let html = '';
-  for (let i = 0; i < allCharacters.length; i++) {
-    html += renderOneCharacter(allCharacters[i]);
+  for (let i = 0; i < character.length; i++) {
+    html += renderOneCharacter(character[i]);
   }
   characterListEl.innerHTML = html;
 
@@ -44,15 +45,41 @@ function renderFavoritesCharacters() {
 
 function handleClick(event) {
   event.currentTarget.classList.toggle('selected');
-  console.log(allCharacters);
+
   const selectedCharacter = allCharacters.find(
     (eachCharacterObj) =>
       eachCharacterObj.char_id === parseInt(event.currentTarget.id)
+    //encontramos el personaje que queremos comparando el id del que hemos seleccionado y el char_id de el del API (le hacemos parseint al currentTarget porque el API viene dado en formato number).
   );
 
-  favoriteCharacters.push(selectedCharacter);
-  renderFavoritesCharacters();
+  const CharacterInFavoritesIndex = favoriteCharacters.findIndex(
+    (eachCharacterObj) =>
+      eachCharacterObj.char_id === parseInt(event.currentTarget.id)
+    //comparamos el index del seleccionado con el del API para después guardarlo y pintarlo en favoritos
+  );
+
+  if (CharacterInFavoritesIndex === -1) {
+    //Si no está en el array, se añade (y se pinta más abajo con la función render)
+    favoriteCharacters.push(selectedCharacter);
+
+    localStorage.setItem('favCharacter', JSON.stringify(favoriteCharacters));
+  } else {
+    //si está ya en favoritos, lo quitamos
+    favoriteCharacters.splice(CharacterInFavoritesIndex, 1);
+
+    localStorage.setItem('favCharacter', JSON.stringify(favoriteCharacters));
+  }
+  renderFavoritesCharacters(); //pinta los favoritos
 }
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const userSearch = input.value.toLowerCase();
+  const filteredCharacters = allCharacters.filter((character) =>
+    character.name.toLowerCase().includes(userSearch)
+  );
+  renderAllCharacters(filteredCharacters);
+});
 
 //ELEMENTOS AL CARGAR LA WEB
 
@@ -61,6 +88,12 @@ fetch('https://breakingbadapi.com/api/characters')
   .then((jsonData) => {
     allCharacters = jsonData;
 
-    renderAllCharacters();
+    renderAllCharacters(allCharacters);
     renderFavoritesCharacters();
   });
+
+const savedFavorites = JSON.parse(localStorage.getItem('favCharacter'));
+if (savedFavorites !== null) {
+  favoriteCharacters = savedFavorites;
+  renderFavoritesCharacters();
+}
