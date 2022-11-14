62c15ff2d8f99d316b846b0281237f6df21436bf
nayraromero <nayraromero@gmail.com>
2022-11-14 13:49:25 +0100

    finish closeIcon

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,42 @@
-/* eslint-disable no-console */
 'use strict';
-// //VARIABLES
+
+//VARIABLES
+
 const btn = document.querySelector('.btn-search');
 const input = document.querySelector('.js-input');
 const characterListEl = document.querySelector('.js-list');
 const favoritesListEl = document.querySelector('.js-list-favorites');
-// const closeIcon = document.querySelector('.icon');
+const resetButton = document.querySelector('.btn-reset');
+
 let allCharacters = [];
 let favoriteCharacters = [];
 
-//FUNCIONES
-function renderOneCharacter(oneCharacter) {
+//RENDER FUNCTIONS
+
+function renderOneCharacter(oneCharacter, iconFavorites) {
+  // Función clave para pintar más adelante allCharacter y favoriteCharacters
   const CharacterInFavoritesIndex = favoriteCharacters.findIndex(
     (eachCharacterObj) =>
       eachCharacterObj.char_id === parseInt(oneCharacter.char_id)
   ); //comparamos el index del personaje y el de favoritos
   let classFavorite = '';
-  let classHidden = '';
+
   if (CharacterInFavoritesIndex === -1) {
     //si no está en favoritos, no se le añade ninguna clase
     classFavorite = '';
-    classHidden = 'hidden';
   } else {
     classFavorite = 'selected';
-    classHidden = '';
     //si el seleccionado está en favoritos, se le añade la clase selected
   }
   let html =
     /* se le añade la clase selected (classFavorite) en función de si el index no es -1 */
     `<li>
   <article class="article ${classFavorite}" id="${oneCharacter.char_id}">
-  <i class="fa-solid fa-circle-xmark icon ${classHidden}"></i>
-    <img
+  `;
+  if (iconFavorites === true) {
+    html += `<i class="fa-solid fa-circle-xmark icon" id="${oneCharacter.char_id}"></i>`;
+  }
+  html += `<img
       src=${oneCharacter.img} class="characterPhoto">
     <h3 class="characterName">${oneCharacter.name}</h3>
     <p class="characterStatus">${oneCharacter.status}</p>
@@ -43,28 +48,43 @@ function renderOneCharacter(oneCharacter) {
 function renderAllCharacters(character) {
   let html = '';
   for (let i = 0; i < character.length; i++) {
-    html += renderOneCharacter(character[i]);
+    html += renderOneCharacter(character[i], false);
   }
   characterListEl.innerHTML = html;
 
   const allArticles = document.querySelectorAll('.article');
 
   for (const articleCharacter of allArticles) {
-    articleCharacter.addEventListener('click', handleClick);
+    articleCharacter.addEventListener('click', keepFavoritesArray);
   }
 }
 
 function renderFavoritesCharacters() {
   let html = '';
   for (let i = 0; i < favoriteCharacters.length; i++) {
-    html += renderOneCharacter(favoriteCharacters[i]);
+    html += renderOneCharacter(favoriteCharacters[i], true);
   }
   favoritesListEl.innerHTML = html;
+
+  const closeIcon = document.querySelectorAll('.icon');
+  for (let i = 0; i < closeIcon.length; i++) {
+    closeIcon[i].addEventListener('click', (event) => {
+      const CharacterInFavoritesIndex = favoriteCharacters.findIndex(
+        (eachCharacterObj) =>
+          eachCharacterObj.char_id === parseInt(event.currentTarget.id)
+      );
+      //comparamos el index del seleccionado con el del API para después eliminarlo de favoritos
+      favoriteCharacters.splice(CharacterInFavoritesIndex, 1);
+      localStorage.setItem('favCharacter', JSON.stringify(favoriteCharacters));
+      renderFavoritesCharacters();
+    });
+  }
 }
 
-function handleClick(event) {
+//DATA FUNCTIONS
+
+function keepFavoritesArray(event) {
   event.currentTarget.classList.toggle('selected');
-  event.currentTarget.classList.toggle('hidden');
 
   const selectedCharacter = allCharacters.find(
     (eachCharacterObj) =>
@@ -90,37 +110,54 @@ function handleClick(event) {
     localStorage.setItem('favCharacter', JSON.stringify(favoriteCharacters));
   }
   renderFavoritesCharacters(); //pinta los favoritos
-
-  // closeIcon.addEventListener('click', () => {
-  //   debugger;
-  //   console.log('has hecho click en la cruz');
-  //   favoriteCharacters.splice(CharacterInFavoritesIndex, 1);
-  //   localStorage.setItem('favCharacter', JSON.stringify(favoriteCharacters));
-  // });
 }
 
-btn.addEventListener('click', (event) => {
-  event.preventDefault();
+function getData() {
+  fetch('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((jsonData) => {
+      allCharacters = jsonData;
+
+      renderAllCharacters(allCharacters);
+      renderFavoritesCharacters();
+    });
+}
+getData();
+
+function savedLocalStorage() {
+  const savedFavorites = JSON.parse(localStorage.getItem('favCharacter'));
+  if (savedFavorites !== null) {
+    favoriteCharacters = savedFavorites;
+    renderFavoritesCharacters();
+  }
+}
+savedLocalStorage();
+
+//BUTTON FUNCTIONS
+
+function searchButton() {
   const userSearch = input.value.toLowerCase();
   const filteredCharacters = allCharacters.filter((character) =>
     character.name.toLowerCase().includes(userSearch)
   );
   renderAllCharacters(filteredCharacters);
-});
+}
 
-//ELEMENTOS AL CARGAR LA WEB
-
-fetch('https://breakingbadapi.com/api/characters')
-  .then((response) => response.json())
-  .then((jsonData) => {
-    allCharacters = jsonData;
-
-    renderAllCharacters(allCharacters);
-    renderFavoritesCharacters();
-  });
-
-const savedFavorites = JSON.parse(localStorage.getItem('favCharacter'));
-if (savedFavorites !== null) {
-  favoriteCharacters = savedFavorites;
+function handleReset() {
+  console.log('has entrado en reset');
+  localStorage.removeItem('favCharacter');
+  favoriteCharacters.length = 0;
   renderFavoritesCharacters();
 }
+
+//EVENTS
+
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+  searchButton();
+});
+
+resetButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  handleReset();
+});
